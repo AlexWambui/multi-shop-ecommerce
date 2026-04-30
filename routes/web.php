69c9c290de2de6f8afef2fdedfc065997a845218
AlexWambui <alexwambuik@gmail.com>
2026-04-30 22:17:45 +0300

    resolving merger conflicts.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+<<<<<<< HEAD
 use App\Http\Controllers\Guest\HomePageController;
 
 Route::get('/', [HomePageController::class, 'homePage'])->name('home');
@@ -9,6 +10,12 @@ Route::get('/', [HomePageController::class, 'homePage'])->name('home');
 Route::inertia('/welcome', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('welcome');
+=======
+
+Route::inertia('/', 'Welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+>>>>>>> 0915937 (first commit after re-install and daisy ui failure.)
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
